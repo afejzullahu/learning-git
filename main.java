f790953e9dced1a25bc12e616117428f7ef7c1ec
Afejzullahu <afejzullahu@citycollege.sheffield.eu>
2020-04-27 14:43:30 +0300

    CREATE MAIN.java

---
+++ main.java
@@ -0,0 +1,23 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student("CSE1234", "Ariot");
+		System.out.println(s1.toString());
+		
+		Student s2 = new Student();
+		System.out.println(s2);
+		
+		Rectangle r1 = new Rectangle(1222.2, 5.2);
+		System.out.println(r1);
+		
+		Rectangle r2 = new Rectangle();
+		System.out.println(r2);
+		
+		Rectangle r3 = new Rectangle(12.12, 7.7);
+		System.out.println(r3);
+
+	}
+
+}
